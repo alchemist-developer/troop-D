d8ad7595031f16c7690eeb4de06b6ce6c35faf5c
Adryel Simon <adryel@live.it>
2022-09-22 15:17:21 -0300

    🚧 project base 🚧

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Im here</h1>
+    </div>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+    colors: {
+        primaryColor: "#FF6C22",
+        secondaryColor: "#F3F3F3",
+        tertiaryColor: "#2EAFB2",
+        quartenaryColor: "#4EC859",
+    },
+    font: {
+        family: {
+            default: "'Advent Pro', sans-serif",
+        }}};
