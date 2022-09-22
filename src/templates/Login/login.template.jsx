e5937e1de1dc895bffc2cc49c43515e224c8b81f
Adryel Simon <adryel@live.it>
2022-09-22 15:46:13 -0300

    feat: initial login template

---
+++ src/templates/Login/login.template.jsx
@@ -0,0 +1,28 @@
+import Box from "../../components/atoms/Box";
+import { theme } from "../../styles/theme";
+
+
+export default function Login() {
+    return (
+
+        <Box
+            bg={theme.colors.primaryColor}
+            width="100%"
+            height="100vh"
+            justify="center"
+            align="center"
+            direction="column"
+        >
+            <Box
+                bg={theme.colors.primaryColor}
+                width="100%"
+                height="100vh"
+                justify="center"
+                align="center"
+                direction="column"
+            >
+
+            </Box>
+        </Box>
+    );
+}
