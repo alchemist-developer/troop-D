4dbd4f9b91d6b0eeb12ba46c79148d9d5df2471b
Adryel Simon <adryel@live.it>
2022-09-23 03:06:33 -0300

    fix: Each child in a list should have a unique "key" prop

--- src/components/organisms/ButtonsList/index.jsx
+++ src/components/organisms/ButtonsList/index.jsx
@@ -14,10 +14,10 @@ export default function ButtonsList() {
             <ul style={{ listStyle: "none", display: "flex", flexDirection: "column", gap: "15px" }}>
                 <li><Button href="/" padding="20px" width="211px" height="50px" text={[<Image src={InicioFrame} key="1" alt="frame" />, "⠀Início"]} color={theme.colors.senaryColor} bg={theme.colors.quinaryColor} /></li>
                 <li><Button padding="20px" width="211px" height="50px" text={[<Image src={RelatorioFrame} key="2" alt="frame" />, "⠀Relatórios"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
-                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="2" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
-                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="2" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
-                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="2" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
-                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="2" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
+                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="3" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
+                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="4" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
+                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="5" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
+                <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="6" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
             </ul>
         </Box>
     )
