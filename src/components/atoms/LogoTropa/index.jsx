9fa6ead337c7a4f4df863bc6f35c6363d64b6e92
Adryel Simon <adryel@live.it>
2022-09-23 03:23:40 -0300

    chore: fix image link

--- src/components/atoms/LogoTropa/index.jsx
+++ src/components/atoms/LogoTropa/index.jsx
@@ -3,6 +3,6 @@ import Logo from "../../../../public/static/Logo.svg"
 
 export default function LogoTropa (props) {
     return (
-        <Image src={Logo} alt="Logo Tropa Digital" width={ props.width } height= {props.height} />
+        <a><Image src={Logo} alt="Logo Tropa Digital" width={ props.width } height= {props.height} /></a>
     );
 }
