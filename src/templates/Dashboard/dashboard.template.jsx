c4a89daf22b1ba8757628542b59b205100bd7bd3
Adryel Simon <adryel@live.it>
2022-09-23 03:23:18 -0300

    fix: <title> on next/head

--- src/templates/Dashboard/dashboard.template.jsx
+++ src/templates/Dashboard/dashboard.template.jsx
@@ -2,6 +2,7 @@ import Box from "../../components/atoms/Box";
 import Header from "../../components/organisms/Header";
 import LeftMenu from "../../components/organisms/LeftMenu"
 import CardList from "../../components/organisms/CardList"
+import Head from "next/head"
 
 import { theme } from "../../styles/theme";
 import * as S from "./dashboard.styled";
@@ -12,6 +13,10 @@ export default function DashboardTemplate() {
     return (
 
         <S.GridParent>
+            <Head>
+                <title>Tropa.Digital | Dashboard</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
 
             <Box padding="60px 30px 0 30px" bg={theme.colors.secondaryColor}>
                 <LeftMenu />
