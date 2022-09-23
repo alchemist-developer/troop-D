ed9d88a36fda8949182e282618cae39e72d3e7d9
Adryel Simon <adryel@live.it>
2022-09-23 05:05:30 -0300

    fix(style): first child button resized

--- src/components/organisms/ButtonsList/index.jsx
+++ src/components/organisms/ButtonsList/index.jsx
@@ -12,7 +12,7 @@ export default function ButtonsList() {
 
         <Box direction="column" justify="center" align="center">
             <ul style={{ listStyle: "none", display: "flex", flexDirection: "column", gap: "15px" }}>
-                <li><Button href="/" padding="20px" width="211px" height="50px" text={[<Image src={InicioFrame} key="1" alt="frame" />, "⠀Início"]} color={theme.colors.senaryColor} bg={theme.colors.quinaryColor} /></li>
+                <li ><Button href="/" padding="20px 20px" width="200%" height="50px" text={[<Image src={InicioFrame} key="1" alt="frame" />, "⠀Início"]} color={theme.colors.senaryColor} bg={theme.colors.quinaryColor} /></li>
                 <li><Button padding="20px" width="211px" height="50px" text={[<Image src={RelatorioFrame} key="2" alt="frame" />, "⠀Relatórios"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
                 <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="3" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
                 <li><Button padding="20px" width="211px" height="50px" text={[<Image src={ContatosFrame} key="4" alt="frame" />, "⠀Contatos"]} color={theme.colors.quinaryColor} bg={theme.colors.tertiaryColor} /></li>
